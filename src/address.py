5cf1b36342d35453c57dc107025fc866a82f9feb
LettaSav <lettawetta@icloud.com>
2025-11-13 13:07:16 +0200

    Add modules docs

---
+++ src/address.py
@@ -0,0 +1,9 @@
+from field import Field
+
+
+# Represents an address field
+class Address(Field):
+    def __init__(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Address must be a string.")
+        super().__init__(value.strip())
---
+++ src/phone.py
@@ -0,0 +1,9 @@
+from field import Field
+
+
+# Represents a phone number with validation: must be exactly 10 digits
+class Phone(Field):
+    def __init__(self, value):
+        if not value.isdigit() or len(value) != 10:
+            raise ValueError("Phone number must be 10 digits.")
+        super().__init__(value)  # ← cleaner and consistente
